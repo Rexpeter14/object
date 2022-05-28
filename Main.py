25166e271463d4d3d9c8ea74e5eb7318ddb02197
Rexpeter14 <105394366+Rexpeter14@users.noreply.github.com>
2022-05-29 00:54:44 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+class student:
+
+  def __init__(mysillyobject, name, age, score):
+
+    mysillyobject.name = string
+
+    mysillyobject.age = integer 
+
+    mysillyobject.score =integer
+
+  def myfunc(abc):
+
+    print("score " )
+
+p1 = student(36)
+
+p1.myfunc()
