ca4fb767d91170fcde62032d62f4e07a2c3c1cd9
Rexpeter14 <105394366+Rexpeter14@users.noreply.github.com>
2022-06-17 10:38:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from ast import Str
+
+
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self,name,age,tracks,score):
+        self.name =str(name)
+        self.age =int(age)
+        self.tracks =list(tracks)
+        self.score =float(score)
+
+    def change_name(self, change_name):
+        self.change_name= change_name
+        print("The student new name is", change_name)
+
+    def change_age(self, change_age):
+        self.change_age= change_age
+        print("The student new age is", change_age) 
+
+    def add_track(self,add_track):
+        self.add_track= add_track
+        print("The student new track is ", add_track)
+
+    def get_score(self):
+        return("The new student age is ",self.score)
+
+
+
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
